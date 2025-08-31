003d765e01fe5bb4652363b48ac830afd32525b3
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-08-31 05:44:00 -0500

    check if number is even or odd

---
+++ section7/CheckIfNumberIsEven.c
@@ -0,0 +1,25 @@
+// Program to determine if a number is even or odd
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int numberToTest;
+    int remainder;
+
+    printf("Enter you number to be tested:\n");
+    scanf("%i", &numberToTest);
+
+    remainder = numberToTest % 2;
+    
+    if (remainder == 0)
+    {
+        printf("Number %i is Even\n", numberToTest);
+    }else
+    {
+        printf("Number %i is Odd\n", numberToTest);
+    }
+    
+
+    return 0;
+}
