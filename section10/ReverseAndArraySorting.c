6a0cb80e27af5a8a5c2340b9133470ba3b2c0ac3
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-09-04 08:02:16 -0500

    adding ordering using bubble sort

--- section10/ReverseAndArraySorting.c
+++ section10/ReverseAndArraySorting.c
@@ -17,12 +17,53 @@ int main(int argc, char const *argv[])
     char stringEntered[MAX_STRING_LENG];
     char stringReversed[MAX_STRING_LENG];
 
+    char name[25][50];
+    char temp[25];
+    int numberStringsEntered = 0;
+
     printf("Please enter a string to be reversed (MAX STRING LENGTH %d): ", MAX_STRING_LENG);
     scanf("%[^\n]%*c", stringEntered);
 
     reverseString(stringEntered, stringReversed);
 
     printf("String entered is: %s\nString reversed is: %s\n", stringEntered, stringReversed);
+
+
+    printf("\n------------------\n");
+    printf("Sort of string of an array using bubble sort: \n");
+    printf("Input numbers of string you want to enter: ");
+    
+    scanf("%d", &numberStringsEntered);
+
+    for (int  i = 0; i < numberStringsEntered; i++)
+    {
+        printf(">:");
+        scanf("%s", name[i]);
+    }
+
+    for (int  i = 0; i <= numberStringsEntered; i++)
+    {
+        for (int j = 0; j  <= numberStringsEntered - 1; j++)
+        {
+            if (strcmp(name[j], name[j + 1]) > 0)
+            {
+                strncpy(temp, name[j], sizeof(temp) - 1);
+                strncpy(name[j], name[j + 1], sizeof(temp) - 1);
+                strncpy(name[j + 1], temp, sizeof(temp) - 1);
+            }
+            
+        }
+        
+    }
+
+    printf("\nWords Ordered:\n");
+    for (int i = 0; i <= numberStringsEntered; i++)
+    {
+        printf("%s\n", name[i]);
+    }
+    
+    
+    
     
 
     return 0;
