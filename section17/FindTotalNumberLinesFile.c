490ea2fca16ed896d299a5a3f5ed3524042d7ad2
Fredy Mosquera <25505457+fmosquera0101@users.noreply.github.com>
2025-09-09 06:26:57 -0500

    calculating number of lines of a file challenge

---
+++ section17/FindTotalNumberLinesFile.c
@@ -0,0 +1,34 @@
+// Write a program to find the total number of lines in a text file
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE *pFile;
+    int character;
+    int counter = 0;
+
+    pFile = fopen("myFile.txt", "r");
+
+    if (pFile == NULL)
+    {
+        printf("Can read the file");
+        return -1;
+    }
+
+    while ((character = fgetc(pFile)) != EOF)
+    {
+        if (character == '\n')
+        {
+            counter++;
+        }
+        
+    }
+
+    counter++;
+
+    printf("Number of lines of Read file is: %i", counter);
+    
+    
+    return 0;
+}
